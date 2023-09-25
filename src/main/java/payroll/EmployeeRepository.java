045a66aac5e23b4410f986b1ced22f640db9f580
BenedictCoding <chang6617@naver.com>
2023-09-25 23:30:53 +0900

    CHAPTER 19 v2.1

--- src/main/java/payroll/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package payroll;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import payroll.entity.Employee;
-
-//public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-//
-//}
